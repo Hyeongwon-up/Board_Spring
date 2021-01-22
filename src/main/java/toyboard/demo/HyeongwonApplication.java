921fac50593b15cce26a1487c0704859836c92d4
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-23 05:05:30 +0900

    First

---
+++ src/main/java/toyboard/demo/HyeongwonApplication.java
@@ -0,0 +1,13 @@
+package toyboard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeongwonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyeongwonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toyboard/demo/HyeongwonApplicationTests.java
@@ -0,0 +1,13 @@
+package toyboard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeongwonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

41cdba9e5e429675a71d9043704977483a061c1a
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-26 03:00:26 +0900

    update

--- src/main/java/toyboard/controller/BoardController.java
+++ src/main/java/toyboard/controller/BoardController.java
@@ -3,10 +3,12 @@ package toyboard.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import toyboard.Service.BoardService;
 import toyboard.dto.BoardDto;
+
+import java.util.List;
 
 @Controller //컨트롤러임을 명
 @AllArgsConstructor
@@ -18,7 +20,6 @@ public class BoardController {
     @GetMapping("/")
     public String list() {
         return "board/list.html";
-
     }
 
     @GetMapping("/post")
@@ -32,4 +33,38 @@ public class BoardController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDTO) {
+        boardService.savePost(boardDTO);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+
+
+
 }
